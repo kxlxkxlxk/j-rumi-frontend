0348636052ce267a0f870a62554999ddc325253d
kxlxkxlxk <mooninn0927@gmail.com>
2026-09-17 13:37:22 +0900

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -205,7 +205,6 @@ export default function Home() {
             <input
               type="file"
               accept="image/*"
-              capture="environment"
               className="hidden"
               onChange={handleFileChange}
             />
